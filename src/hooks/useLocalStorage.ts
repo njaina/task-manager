b835e6127cdc7e0d0e84050db1692a291b7efa33
njaina <hei.njaina.2@gmail.com>
2023-06-15 11:55:57 +0300

    feat: implement one more hooks

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,19 @@
-const useLocalStorage = () => {
+import { useState } from 'react';
 
-}
+const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
+  // Lire la valeur initiale à partir du Local Storage
+  // ou utiliser l'initialValue fournie
+  const storedValue = localStorage.getItem(key);
+  const initialStoredValue = storedValue ? JSON.parse(storedValue) : initialValue;
+  const [value, setValue] = useState<T>(initialStoredValue);
 
-export {
-  useLocalStorage
-}
+  // Mettre à jour la valeur dans le Local Storage chaque fois qu'elle change
+  const updateValue = (newValue: T) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, updateValue];
+};
+
+export default useLocalStorage;
