74c52cde18216af96a17c94506f5d32034990d7c
njaina <hei.njaina.2@gmail.com>
2023-06-15 11:36:35 +0300

    feat: implement the diffTime function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,43 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { format } from "date-fns";
+
+interface TimeDifference {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 /**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
+ * Calculates the time difference between the server time and client time.
+ * @param {Date} serverTime - The server time.
+ * @param {Date} clientTime - The client time.
+ * @returns {TimeDifference} The time difference object containing { hours, minutes, seconds }.
+ */
+const calculateTimeDifference = (serverTime: Date, clientTime: Date): TimeDifference => {
+  const timeDiff = Math.abs(serverTime.getTime() - clientTime.getTime());
+  const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
+  return { hours, minutes, seconds };
+};
 
-export default function Home() {
+interface HomeProps {
+  server: string;
+}
+
+const Home = ({ server }: HomeProps): JSX.Element => {
   const router = useRouter();
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+
+  const client = new Date();
+  const serverTime = new Date(server);
+
+  const timeDiff = calculateTimeDifference(serverTime, client);
+
   return (
     <>
       <Head>
@@ -24,18 +47,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
+          {/* Display the server date and time */}
           <p>
-            Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Server datetime: <span className="serverTime">{format(serverTime, "dd/MM/yyyy, HH:mm:ss")}</span>
           </p>
 
-          {/* Display here the time difference between the server side and the client side */}
+          {/* Display the time difference between the server and the client */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">
+              {timeDiff.hours} hours, {timeDiff.minutes} minutes, {timeDiff.seconds} seconds
+            </span>
           </p>
         </div>
 
@@ -45,4 +68,16 @@ export default function Home() {
       </main>
     </>
   );
+};
+
+export default Home;
+
+export async function getServerSideProps() {
+  const server = new Date().toString();
+
+  return {
+    props: {
+      server: server,
+    },
+  };
 }
